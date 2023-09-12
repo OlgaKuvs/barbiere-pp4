8ec6b8eecec19269a35e621689d225676ae99139
OlgaKuvs <olja_kuv@inbox.lv>
2023-09-12 21:27:48 +0100

    chore: change ALLOWED_HOSTS

--- barbiere/settings.py
+++ barbiere/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['barbiere..herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['barbiere-e0b7941f6ea4.herokuapp.com', 'localhost']
 
 
 # Application definition
