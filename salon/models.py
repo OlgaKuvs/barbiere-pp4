d4f0b31d3ad8bc5b14e54c771660c70d5550ccf5
OlgaKuvs <olja_kuv@inbox.lv>
2023-09-14 12:46:05 +0100

    feat: add service, barber and working hours models

--- salon/models.py
+++ salon/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Service(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    duration = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+    
+
+class Barber(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    is_available = models.BooleanField(default=True, null=False, blank=False) 
+
+    def __str__(self):
+        return self.name
+    
+
+class Working_hours(models.Model):
+    barber = models.ForeignKey(to=Barber, on_delete=models.CASCADE)
+    time_start = models.DateTimeInput(auto_now=False, auto_now_add=False)
+    time_end = models.DateTimeInput(auto_now=False, auto_now_add=False)
+
+
